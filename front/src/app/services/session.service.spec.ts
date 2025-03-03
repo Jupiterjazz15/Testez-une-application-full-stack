ee28811f2edf3fedaf0211ef5eb60de627ffd21b
coraliehaller <haller.coralie.15@gmail.com>
2025-03-03 21:21:24 +0100

    session.service.spec.ts : ajout des tests

--- front/src/app/services/session.service.spec.ts
+++ front/src/app/services/session.service.spec.ts
@@ -2,16 +2,75 @@ import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { SessionService } from './session.service';
+import { SessionInformation } from '../interfaces/sessionInformation.interface';
 
-describe('SessionService', () => {
-  let service: SessionService;
+describe('SessionService Unit Tests Suite', () => {
+  let sessionService: SessionService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(SessionService);
+    sessionService = TestBed.inject(SessionService);
   });
 
+  // TEST UNITAIRE
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(sessionService).toBeTruthy();
+  });
+
+  // TESTS D'INTEGRATION
+  it("shouldn't be logged in at first", done => {
+
+    sessionService.$isLogged().subscribe(value => {
+      expect(value).not.toBe(true);
+      expect(sessionService.sessionInformation).toBeFalsy();
+      done();
+    });
+
+  });
+
+  it("should log the user in", done => {
+
+    const user: SessionInformation = {
+      token: "token",
+      type: "type",
+      id: 1,
+      username: "username",
+      firstName: "firstName",
+      lastName: "lastName",
+      admin: false
+    };
+
+    sessionService.logIn(user);
+    sessionService.$isLogged().subscribe(value => {
+      expect(value).toBe(true);
+      expect(sessionService.sessionInformation).toEqual(user);
+      done();
+    });
+
+  });
+
+  it("should log the user out", done => {
+
+    const user: SessionInformation = {
+      token: "token",
+      type: "type",
+      id: 1,
+      username: "username",
+      firstName: "firstName",
+      lastName: "lastName",
+      admin: false
+    };
+
+    sessionService.sessionInformation = user;
+    sessionService.isLogged = true;
+
+    sessionService.logOut();
+
+    sessionService.$isLogged().subscribe(value => {
+      expect(value).toBe(false);
+      expect(sessionService.sessionInformation).toBeFalsy();
+      done();
+    });
+
   });
 });
