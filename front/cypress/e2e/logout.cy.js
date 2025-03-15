fd10780212b9645ac5487062c74a0f0279ff9db4
coraliehaller <haller.coralie.15@gmail.com>
2025-03-15 11:46:00 +0100

    logout.cy.js : ajout d'un test E2E

---
+++ front/cypress/e2e/logout.cy.js
@@ -0,0 +1,21 @@
+describe('Logout Test', () => {
+  beforeEach(() => {
+    cy.visit('/login');
+
+    // 🔹 Connexion de l'utilisateur
+    cy.get('input[formControlName=email]').type('metest@test.com');
+    cy.get('input[formControlName=password]').type('azerty{enter}{enter}');
+
+    // 🔹 Vérification de la redirection vers /sessions
+    cy.url().should('include', '/sessions');
+    cy.wait(500);
+  });
+
+  it('should log out the user and redirect to /login', () => {
+    // 🔹 Cliquer sur Logout
+    cy.get('[data-cy="nav-logout"]').click();
+
+    // 🔹 Vérifier la redirection vers /login
+    cy.url().should('include', '/login');
+  });
+});
